c59008db1b320055bae5b8a80936f3bca4674bfd
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-29 09:38:11 +0530

    kotlin: classes and interfaces

---
+++ day-03-kotlin-adv/src/main/kotlin/car.kt
@@ -0,0 +1,11 @@
+package org.example
+
+interface LightSensors
+
+interface GPSListener
+
+open class Vehicle
+
+open class Car
+
+class Bwm : Car(), GPSListener, LightSensors
---
+++ day-03-kotlin-adv/src/main/kotlin/splcls.kt
@@ -0,0 +1,11 @@
+package org.example
+
+data class SysFile(
+    val name: String,
+    val path: String
+)
+
+class FileUtil {
+    fun readFile() {}
+    fun writeFile() {}
+}
