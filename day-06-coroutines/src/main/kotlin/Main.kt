5b46500df7592a02f2a2ee8e7f844b6e72e9d0a7
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-05 10:16:50 +0530

    kotlin: coroutines demo: suspended functions

--- day-06-coroutines/src/main/kotlin/Main.kt
+++ day-06-coroutines/src/main/kotlin/Main.kt
@@ -1,15 +1,19 @@
 package org.example
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
+
     println("Hello World!")
 
     runBlocking { // Coroutine Scope
+        concurrency()
+    }
+    println("Hi Hello")
+}
 
+suspend fun basics() {
+    coroutineScope {
         launch { // coroutine
             delay(2000)
             println("Launch: Hey hi")
@@ -21,6 +25,42 @@ fun main() {
             delay(1000)
             println("Async: Hey hi")
         }
+
+        sayHiLater()
     }
-    println("Hi Hello")
+}
+
+suspend fun sayHiLater() {
+    println("Suspended Function")
+    coroutineScope {
+        launch {
+            delay(1000L)
+            println("World")
+        }
+        println("Hello")
+    }
+}
+
+suspend fun concurrency() {
+    coroutineScope {
+
+        val jb = launch {  }
+
+        val task1 = async {
+            delay(2000)
+            "task 1: hello"
+        }
+
+        val task2 = async {
+            delay(1000)
+            "task2: hi"
+        }
+
+        val res1 = task1.await()
+        println("✅ Task1 Completed")
+        val res2 = task2.await()
+        println("✅ Task2 Completed")
+
+        println("Results $res1, $res2")
+    }
 }
