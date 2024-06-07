7838337683b6c4e0f87672f752a8fa42ecda9383
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-07 10:56:12 +0530

    android : coroutine counter

--- day-08-android-ui/SimpleUiApp/app/src/main/java/com/ani/android/ui/MainActivity.kt
+++ day-08-android-ui/SimpleUiApp/app/src/main/java/com/ani/android/ui/MainActivity.kt
@@ -15,6 +15,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+   private val scp = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,15 +26,28 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text)
         textView.text = "Setting without Co-routine"
 
-        val scp = CoroutineScope(Dispatchers.Main)
         scp.launch {
             delay(2000)
-            textView.text = "Changing from code"
+            textView.text = "Changing from code ${mult(12, 89)}"
         }
 
         val btn = findViewById<Button>(R.id.button)
         btn.setOnClickListener {
-            textView.text = "Setting on Btn Click"
+//            textView.text = "Setting on Btn Click ${mult(12, 89)}"
+            counter(textView)
+        }
+    }
+
+    private fun mult(n1: Int, n2: Int) : String {
+        return "${n1 * n2}"
+    }
+
+    private fun counter(textView: TextView) {
+        scp.launch {
+            for (i in 10 downTo 1) {
+                delay(1000)
+                textView.text = "$i"
+            }
         }
     }
 }
