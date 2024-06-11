2b6f0dbefc8051056a7bcc69692d9c5aea2e465f
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-11 10:49:16 +0530

    android : data binding : events

--- day-10/DataBinding/app/src/main/java/com/ani/android/databinding/MainActivity.kt
+++ day-10/DataBinding/app/src/main/java/com/ani/android/databinding/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ani.android.databinding
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.ani.android.databinding.databinding.ActivityMainBinding
+
+class BtnClickHandler(
+    private val lstnr : () -> Unit
+) {
+    fun onBtnClk() {
+        Log.i("@ani","Button Clicked")
+        lstnr()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +33,11 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
 
-        binding.sd = SensorEvent("GPS", "2022-01-01 09:00", true)
+        val sd = SensorEvent("GPS", "2022-01-01 09:00", true)
+        binding.sd = sd
+        binding.btnHnd = BtnClickHandler {
+            binding.sd = SensorEvent("default", "0000-00-00", false)
+        }
+
     }
 }
