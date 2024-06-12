deb77bbd7be94c96e8294e9f8214194668056aea
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-12 10:38:31 +0530

    android : fragment and data binding

--- day-11/FragmentLifeAware/app/src/main/java/com/ani/android/life/ui/BlankFragment.kt
+++ day-11/FragmentLifeAware/app/src/main/java/com/ani/android/life/ui/BlankFragment.kt
@@ -1,9 +1,38 @@
 package com.ani.android.life.ui
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.ani.android.life.ui.databinding.FragmentBlankBinding
+import kotlinx.coroutines.launch
 
 
 class BlankFragment : Fragment() {
+
+    private val viewModel: BlankViewModel = BlankViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentBlankBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_blank, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewModel
+
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            viewModel.blankState.collect {
+//                binding.invalidateAll()
+//            }
+//        }
+
+        return binding.root
+    }
 
 }
--- day-11/FragmentLifeAware/app/src/main/java/com/ani/android/life/ui/BlankViewModel.kt
+++ day-11/FragmentLifeAware/app/src/main/java/com/ani/android/life/ui/BlankViewModel.kt
@@ -1,12 +1,11 @@
 package com.ani.android.life.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.material.badge.BadgeState
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
 
 data class BlankState(
     val text: String
@@ -15,4 +14,11 @@ class BlankViewModel : ViewModel() {
 
     private val _blankState: MutableStateFlow<BlankState> = MutableStateFlow(BlankState(""))
     val blankState: StateFlow<BlankState> = _blankState
+
+//    private val _blankState: MutableLiveData<BlankState> = MutableLiveData(BlankState(""))
+//    val blankState:LiveData<BlankState> = _blankState
+
+    fun handleClick() {
+        _blankState.value = BlankState("Hey Hi")
+    }
 }
