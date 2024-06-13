9fd820091d167d15c04e9db64d5debedc6f0a41d
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-13 12:17:51 +0530

    android: navigation with fragments : passing data

--- day-12/Navigation/app/src/main/java/com/ani/android/navapp/HomeFragment.kt
+++ day-12/Navigation/app/src/main/java/com/ani/android/navapp/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 
 class HomeFragment : Fragment() {
@@ -23,21 +24,35 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        HomeFragmentDirections.actionHomeToUsersFragment()
+
 
         view.findViewById<ImageView>(R.id.imageView).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_home_to_usersFragment)
+//            findNavController().navigate(R.id.action_home_to_usersFragment)
+
+            val action = HomeFragmentDirections.actionHomeToUsersFragment()
+            findNavController().navigate(action)
         }
 
         view.findViewById<ImageView>(R.id.imageView2).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_invoicesFragment2)
+//            findNavController().navigate(R.id.action_homeFragment_to_invoicesFragment2)
+
+            val invId = 100
+            val action = HomeFragmentDirections.actionHomeFragmentToInvoicesFragment2(invId)
+
+            findNavController().navigate(action)
        }
 
         view.findViewById<ImageView>(R.id.imageView3).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_billsFragment)
+//            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_billsFragment)
+            val action = HomeFragmentDirections.actionHomeFragmentToBillsFragment()
+            findNavController().navigate(action)
         }
 
         view.findViewById<ImageView>(R.id.imageView4).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_settingsFragment)
+//            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_settingsFragment)
+            val action = HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+            findNavController().navigate(action)
         }
 
     }
