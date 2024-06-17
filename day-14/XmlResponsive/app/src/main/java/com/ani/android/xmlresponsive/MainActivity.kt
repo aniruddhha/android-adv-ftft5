32ac266a5b5ae67cd9bbf556ad75cdc547c48f4b
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-17 11:44:24 +0530

    XMl Responsive App

---
+++ day-14/XmlResponsive/app/src/main/java/com/ani/android/xmlresponsive/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ani.android.xmlresponsive
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).setText("Hey Hi")
+
+        findViewById<TextView>(R.id.textView3)?.apply {
+            setText("I am description")
+        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
