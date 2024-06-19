d28e0f66f5372b33da01d620e57b9765d51f591b
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-19 12:36:32 +0530

    Broadcast receivers: registered in xml

--- day-16/BinderDemo/app/src/main/java/com/ani/android/binder/MainActivity.kt
+++ day-16/BinderDemo/app/src/main/java/com/ani/android/binder/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,11 +23,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import com.ani.android.binder.ui.theme.BinderDemoTheme
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class MyRepository(
     private val service: MyBinderService
@@ -43,34 +48,59 @@ class Vm(
     private val _channel: Channel<Int> = Channel(0)
     val flow: Flow<Int> = _channel.receiveAsFlow()
 
-    fun handleButtonClick() {
-        _channel.trySend(repository.nextInt)
+    fun handleButtonClick(): Int {
+        return repository.nextInt
     }
 }
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var  mService: MyBinderService
+    private lateinit var vm: Vm
+    private val _channel: Channel<Int> = Channel<Int>()
+    private val flow: Flow<Int> = _channel.receiveAsFlow()
+
     private var mBound = false
+
     private val conn : ServiceConnection = object: ServiceConnection {
         override fun onServiceConnected(name: ComponentName, service: IBinder) {
             val binder = service as MyBinderService.LocalBinder
             mService = binder.getServiceInstance()
             mBound = true
+
+            Log.i("@ani", "Service Connected")
+            val repository = MyRepository(mService)
+            vm = Vm(repository)
+            _channel.trySend(100)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
             mBound = false
+            Log.i("@ani", "Service DisConnected")
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        startService(Intent(this, MyBinderService::class.java))
+
+        lifecycleScope.launch {
+            flow.collect {
+                Log.i("@ani", "$it")
+                if(it == 100) {
+                    Log.i("@ani", "Got 100")
+                    renderLayout()
+                }
+            }
+        }
+    }
+
+    private fun renderLayout() {
         setContent {
             BinderDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting()
+                    Greeting(vm)
                 }
             }
         }
@@ -78,8 +108,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onStart() {
         super.onStart()
-
-        bindService(Intent(this, MyBinderService::class.java), conn, BIND_AUTO_CREATE)
+        bindService(
+            Intent(this, MyBinderService::class.java), conn, BIND_AUTO_CREATE
+        )
     }
 
     override fun onStop() {
@@ -93,7 +124,6 @@ class MainActivity : ComponentActivity() {
 fun Greeting(
     vm: Vm
 ) {
-
     val (value, setValue) = remember { mutableIntStateOf(0) }
 
     Column {
@@ -104,5 +134,4 @@ fun Greeting(
             Text(text = "Next")
         }
     }
-
 }
--- day-16/BinderDemo/app/src/main/java/com/ani/android/binder/MyBinderService.kt
+++ day-16/BinderDemo/app/src/main/java/com/ani/android/binder/MyBinderService.kt
@@ -4,9 +4,8 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 import android.widget.Toast
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import java.util.Random
 
 class MyBinderService : Service() {
@@ -19,15 +18,21 @@ class MyBinderService : Service() {
 //        get() = MutableStateFlow<Int>(mGen.nextInt())
 
     val randomNumber: Int
-        get() =mGen.nextInt()
+        get() = mGen.nextInt()
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("@ani", "Service On STart Command")
+        return super.onStartCommand(intent, flags, startId)
+    }
 
     override fun onBind(intent: Intent?): IBinder {
+        Log.i("@ani", "In Service On Bind")
         Toast.makeText(this, "Bind", Toast.LENGTH_SHORT).show()
         return  binder
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
+        Log.i("@ani", "In Service On UnBind")
         Toast.makeText(this, "UnBind", Toast.LENGTH_SHORT).show()
         return super.onUnbind(intent)
     }
