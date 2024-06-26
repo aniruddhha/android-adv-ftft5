3dc843d26f2cfca5918096f453552165997e9df0
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-06-26 12:37:58 +0530

    android : sensors : gyroscope

--- day-21/SensorApp/app/src/main/java/com/ani/android/sensor/MainActivity.kt
+++ day-21/SensorApp/app/src/main/java/com/ani/android/sensor/MainActivity.kt
@@ -5,11 +5,19 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Half
 import android.util.Log
 import androidx.activity.ComponentActivity
 import kotlinx.coroutines.flow.Flow
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity(), SensorEventListener {
+
+    private val NS2S = 1.0f / 1000000000.0f
+    private val deltaRotationVector = FloatArray(4) { 0f }
+    private var timestamp: Float = 0f
 
     private lateinit var sensorManager: SensorManager
 
@@ -22,20 +30,57 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
 //        Log.i("@ani", "Light ${sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)}")
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) == null) {
             Log.i("@ani", "Sensor Available")
+            return
         }
 
-        val light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        val light = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
         sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
         Log.i("@ani", "${event?.values?.size}")
 
-        val lux: Float? = event?.let { it.values[0] }
-        Log.i("@ani", "$lux")
+//        val lux: Float? = event?.let { it.values[0] }
+//        Log.i("@ani", "$lux")
 
+        if (timestamp != 0f && event != null) {
+            val dT = (event.timestamp - timestamp) * NS2S
+            // Axis of the rotation sample, not normalized yet.
+            var axisX: Float = event.values[0]
+            var axisY: Float = event.values[1]
+            var axisZ: Float = event.values[2]
+
+            // Calculate the angular speed of the sample
+            val omegaMagnitude: Float = sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ)
+
+            // Normalize the rotation vector if it's big enough to get the axis
+            // (that is, EPSILON should represent your maximum allowable margin of error)
+            if (omegaMagnitude > Half.EPSILON) {
+                axisX /= omegaMagnitude
+                axisY /= omegaMagnitude
+                axisZ /= omegaMagnitude
+            }
+
+            // Integrate around this axis with the angular speed by the timestep
+            // in order to get a delta rotation from this sample over the timestep
+            // We will convert this axis-angle representation of the delta rotation
+            // into a quaternion before turning it into the rotation matrix.
+            val thetaOverTwo: Float = omegaMagnitude * dT / 2.0f
+            val sinThetaOverTwo: Float = sin(thetaOverTwo)
+            val cosThetaOverTwo: Float = cos(thetaOverTwo)
+            deltaRotationVector[0] = sinThetaOverTwo * axisX
+            deltaRotationVector[1] = sinThetaOverTwo * axisY
+            deltaRotationVector[2] = sinThetaOverTwo * axisZ
+            deltaRotationVector[3] = cosThetaOverTwo
+        }
+        timestamp = event?.timestamp?.toFloat() ?: 0f
+        val deltaRotationMatrix = FloatArray(9) { 0f }
+        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+        // User code should concatenate the delta rotation we computed with the current rotation
+        // in order to get the updated rotation.
+        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
